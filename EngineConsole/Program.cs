a03f58b9cb673e3ac0aadacd45e36df0d7ac0284
E0370 <732625@student.inholland.nl>
2026-05-11 14:50:54 +0200

    Console entry point added

--- EngineConsole/Program.cs
+++ EngineConsole/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EngineLibrary;
+
+var httpClient = new HttpClient();
+var orderService = new OrderService(httpClient);
+// Fetch in-progress orders and calculate the top five products based on total quantity
+var orders = await orderService.GetInProgressOrders();
+var topfive = orderService.GetTopFiveProducts(orders);
+
+foreach (var product in topfive)
+{
+    Console.WriteLine($"Product: {product.ProductName}, GTIN: {product.Gtin}, Total Quantity: {product.TotalQuantity}");
+}
