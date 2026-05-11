95bc087fb68dfba5c207be91778480e010a53abf
E0370 <732625@student.inholland.nl>
2026-05-11 15:41:59 +0200

    Web App controller and view added

--- EngineWeb/Controllers/HomeController.cs
+++ EngineWeb/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
-using EngineWeb.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
-
+using EngineLibrary;
 namespace EngineWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+      public async Task<IActionResult> Index()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            var httpClient = new HttpClient();
+            var orderService = new OrderService(httpClient);
+            var orders = await orderService.GetInProgressOrders();
+            var topfive = orderService.GetTopFiveProducts(orders);
+            return View(topfive);
         }
     }
 }
